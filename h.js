263d10938350c137c25b28378b5b7490cc1e6c6e
Ravi Singh <ravikrishnasingh@gmail.com>
2015-08-16 16:26:36 -0400

    coverage init

--- h.js
+++ h.js
@@ -1,99 +1,50 @@
-// HARMOVY
+// HARMONY
+
 var _ = require("underscore");
-var Sound = require("./sound.js");
-var NoteDictionary = require("./data/notedata.js");
+var m = require("./lib/note-manager");
 
-var activeNotes = {};
-
-var config = {
-    gain: 0.15,
-    attack: 0.1,
-    release: 0.1,
-    detune: null,
-    wave: {
-        type: "sine",
-        data: null // wavetable data
-    }
+/*
+// split DSL for note names
+// get note data for each note
+// and play them
+*/
+var play = function(dsl) {
+  m.getNoteIds(dsl).map(m.getNote).forEach(m.playNote);
 };
 
-var getNoteIds = function(noteDsl) {
-    return noteDsl.split(",").map(function(id) {
-        return id.trim();  
-    });
+/*
+// split DSL for note names
+// if DSL is not present, stop all notes
+// otherwise stop playing notes specified
+*/
+var stop = function(dsl) {
+  if(dsl) m.getNoteIds(dsl).map(m.getActiveNote).forEach(m.stopNote);
+  else m.getActiveNotes().forEach(m.stopNote);
 };
 
-var getNote = function(noteId) {
-    var name = noteId.slice(0, -1);
-    var octave = noteId.slice(-1);
-    return NoteDictionary.getNote({
-        name: name,
-        octave: parseInt(octave)
-    });
+/*
+// update configuration object
+// also updates all active notes
+*/
+var configure = function(options) {
+  config = _.extend(m.config, options);
+  getActiveNotes().forEach(m.configure);
 };
 
-var getActiveNote = function(noteId) {
-    return {noteId: noteId, note: activeNotes[noteId]};
+/*
+// get note IDs for active notes
+*/
+var activeNoteIds = function() {
+  return _.keys(m.activeNotes);
 };
 
-var getId = function(noteData) {
-    return noteData.name + noteData.octave;
+module.exports = {
+  play: play,
+  stop: stop,
+  configure: configure,
+  configuration: m.config,
+  activeNotes: m.activeNotes,
+  activeNoteIds: activeNoteIds
 };
 
-var playNote = function(noteData) {
-    var sound = new Sound();
-    var noteId = getId(noteData);
-    activeNotes[noteId] = sound;
-    configure(getActiveNote(noteId));
-    sound.play(noteData.frequency);
-};
 
-var stopNote = function(activeNote) {
-    if(activeNote) activeNote.note.stop();
-    delete activeNotes[activeNote.noteId];
-};
-
-var configure = function(activeNote) {
-    var sound = activeNote.note;
-    sound.amplitude = config.gain;
-    sound.attackTime = config.attack;
-    sound.releaseTime = config.release;
-    sound.detune(config.detune);
-    sound.waveType(config.wave.type);
-    if(config.wave.data) sound.setWaveTable(config.wave.data);
-};
-
-var getActiveNotes = function() {
-    return _.keys(activeNotes).map(function(key) {
-        return {noteId: key, note: activeNotes[key]};
-    });
-};
-
-var h = {
-    play: function(dsl) {
-        getNoteIds(dsl).map(getNote).forEach(playNote);
-    },
-
-    stop: function(dsl) {
-        if(dsl) getNoteIds(dsl).map(getActiveNote).forEach(stopNote);
-        else getActiveNotes().forEach(stopNote);
-    },
-
-    configure: function(options) {
-       config = _.extend(config, options);
-       getActiveNotes().forEach(configure);
-    },
-
-    configuration: function() {
-        return config;
-    },
-
-    activeNotes: function() {
-        return activeNotes;
-    },
-
-    activeNoteIds: function() {
-        return _.keys(activeNotes);
-    }
-};
-
-module.exports = h;
